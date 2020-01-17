84162d86a9a29937b6ba39581e33a00bc076aece
Sanganu <kppkmsanu@gmail.com>
2020-01-17 16:01:55 -0700

    Quiz working

---
+++ assets/script.js
@@ -0,0 +1,96 @@
+// DOM Elements
+let ele_divcontainer = document.getElementById("quizContainer");
+let start_button = document.getElementById("startQuiz");
+let ele_div = document.getElementById("box");
+let question_div = document.querySelector("#displayQuestion");
+let option1_button = document.getElementById("choice1");
+let option2_button = document.getElementById("choice2");
+let timer_p = document.getElementById("timer");
+let timerID = setTimeout(mytimer,1000);
+let timevalue = 150;
+let container = document.getElementById("quizSection") ;
+let ptag = document.createElement("p");
+
+// Variables
+var timer =50;
+let i = 0;
+let wins = 0;
+let loss = 0;
+
+// Add Event Listeners
+start_button.addEventListener("click",startGame);
+
+ 
+function displayQuestion(){
+  // let question_h1 = document.createElement("h1");
+  // console.log(question_div)
+  // question_h1.textContent = questions[i].question;
+  // question_div.appendChild(question_h1);
+  // option1_button.textContent = questions[i].options[0];
+  // option1_button.setAttribute("data-answer",questions[i].answer)
+  container.textContent = ""
+  ptag.textContent = questions[i].question;
+  let button1 = document.createElement("button");
+  button1.setAttribute("value",questions[i].options[0]);
+  button1.setAttribute("answer",questions[i].answer);
+  button1.setAttribute("class","optionclick");
+  button1.textContent = questions[i].options[0];
+  button1.onclick = checkoptionselected;
+  let button2 = document.createElement("button");
+  button2.setAttribute("value",questions[i].options[1]);
+  button2.setAttribute("answer",questions[i].answer);
+  button2.textContent = questions[i].options[1];
+  button2.setAttribute("class","optionclick");
+  button2.onclick = checkoptionselected;
+  container.appendChild(ptag);
+  container.appendChild(button1);
+  container.appendChild(button2)
+}
+
+function startGame(){
+   console.log("Game")
+   i =0;
+   timevalue = 150;
+   let interval = setInterval(mytimer,50);
+   displayQuestion();
+}
+
+function mytimer(){
+  if (timevalue > 0){
+    timevalue--;
+    timer_p.textContent = timevalue
+  }
+  else{
+    displayResults();
+  }
+}
+
+function checkoptionselected(event){
+  let userchoice = this.value;
+  let answer = this.getAttribute("answer");
+  console.log("USer",userchoice,"answer",answer);
+  if ( userchoice === answer){
+    wins++
+    timevalue = timevalue+10;
+  }
+  else{
+    loss++;
+    timevalue = timevalue - 15;
+  }
+  if (timevalue <= 0)
+  { console.log("Time up");
+    alert("Time up")
+    displayResults();
+  }
+  if (i< questions.length-1){
+    i++;
+    displayQuestion();
+  }
+  else{
+    displayResults();
+  }
+}
+
+function displayResults(){
+   container.textContent = `Results wins - ${wins} : Loss - ${loss}`
+}
