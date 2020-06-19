dd1af09ab8c09de75fc450af0e7343805885f929
Sanganu <kppkmsanu@gmail.com>
2020-06-18 22:08:42 -0600

    List elements moving questions

--- quizGameEasy/resources/jsscript.js
+++ quizGameEasy/resources/jsscript.js
@@ -3,33 +3,60 @@
 const questcont = document.getElementById("question");
 const gameContainer = document.querySelector("#gameContainer");
 const startbut  = document.getElementById("start-game");
+const option1 = document.getElementById("option-1");
+const option2 = document.getElementById("option-2");
+const option3 = document.getElementById("option-3");
+const option4 = document.getElementById("option-4");
+const alloptions = document.getElementsByClassName("options");
+let rightList = document.getElementById("rightlist");
+let wrongList = document.getElementById("wronglist");
 
 //Hide Blocks
 gameContainer.style.display = "none";
-
+for(let i=0;i<alloptions.length;i++){
+    alloptions[i].onclick = validateUserResponse
+}
 // console.log(questions)
 //Global Variables
 let currentQuiz = 0;
 
 //OnClick Event Listeners
 startbut.addEventListener("click",function(){
-    questcont.style.display = "block";
+    gameContainer.style.display = "block";
     startbut.style.display="none";
     displayQuestion()
 });
 
 
 function displayQuestion(){
-    console.log(questions[currentQuiz].question)
-    let qtext = document.createElement("h5");
-    qtext.textContent = questions[currentQuiz].question;
-    let option1 = document.createElement("button");
-    option1.textContent = questions[currentQuiz].options[0];
-    option1.setAttribute("answer",questions[currentQuiz].answer);
-    let option2 = document.createElement("button");
-    option2.textContent = questions[currentQuiz].options[1];
-    option2.setAttribute("answer",questions[currentQuiz].answer);
-    questcont.appendChild(qtext);
-    questcont.appendChild(option1);
-    questcont.appendChild(option2);
+    questcont.textContent = questions[currentQuiz].question;
+    option1.textContent = questions[currentQuiz].choices[0];
+    option2.textContent = questions[currentQuiz].choices[1];
+    option3.textContent = questions[currentQuiz].choices[2];
+    option4.textContent = questions[currentQuiz].choices[3];
+}
+
+
+function validateUserResponse(){
+    console.log("This",this.getAttribute("option-value"))
+    var userAnswer = parseInt(this.getAttribute("option-value"))
+    if( userAnswer === questions[currentQuiz].answer ){
+        let textElement = document.createElement("p")
+        textElement.textContent = questions[currentQuiz].question
+        textElement.classList.add("text-center")
+        rightList.appendChild(textElement)
+    }
+    else{
+        let textElement = document.createElement("p")
+        textElement.textContent = questions[currentQuiz].question
+        textElement.classList.add("text-center")
+        wrongList.appendChild(textElement)
+    }
+    if(currentQuiz<questions.length-1){
+        currentQuiz++;
+        displayQuestion();
+    }
+    else{
+        console.log("End")
+    }
 }
