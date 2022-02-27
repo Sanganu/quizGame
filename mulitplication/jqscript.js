f3ec2db404702601af9c849bdc80d2e8843058d8
Sanganu <kppkmsanu@gmail.com>
2022-02-26 21:55:09 -0700

    add alignment

--- mulitplication/jqscript.js
+++ mulitplication/jqscript.js
@@ -1,23 +1,30 @@
 const quizContEl = document.getElementById("quiz-container");
 const startBtnEl = document.getElementById("start-quiz");
-const h4El = document.createElement("h4")
+const timerEl = document.getElementById("timer")
+const scoreEl = document.getElementById("score")
+const h4El = document.createElement("h4");
+const h5El = document.createElement("h5");
 const button1El = document.createElement("button");
 const button2El = document.createElement("button");
 const button3El = document.createElement("button");
 const button4El = document.createElement("button");
 
 function renderHTMLElements() {
-//     const h3El = document.createElement("h3");
-//     quizContEl.appendChild(h3El);
-//     for (let i = 1; i <= 4; i++) {
-//         const btn1El = document.createElement("button");
-//         quizContEl.appendChild(btn1El);
-//     }
-quizContEl.appendChild(h4El)
-quizContEl.appendChild(button1El);
-quizContEl.appendChild(button2El);
-quizContEl.appendChild(button3El);
-quizContEl.appendChild(button4El);
+        //     const h3El = document.createElement("h3");
+        //     quizContEl.appendChild(h3El);
+        //     for (let i = 1; i <= 4; i++) {
+        //         const btn1El = document.createElement("button");
+        //         quizContEl.appendChild(btn1El);
+        //     }
+            // Quiz Container
+            quizContEl.appendChild(h4El)
+            quizContEl.appendChild(button1El);
+            quizContEl.appendChild(button2El);
+            quizContEl.appendChild(button3El);
+            quizContEl.appendChild(button4El);
+            
+            //Timers and Scores
+
 }
 
 
@@ -27,16 +34,20 @@ let timerObject;
 let score=0;
 
 
+scoreEl.classList.add("hide");
+timerEl.classList.add("hide")
 
 
 startBtnEl.addEventListener("click", function () {
   startBtnEl.classList.add("hide")
   renderHTMLElements();
-  getQuestion()
+  getQuestion();
+  startTimerScore()
 })
 
 function getQuestion(){
     h4El.textContent = questions[questionIndex].question;
+    h4El.classList.add("question")
     button1El.textContent = questions[questionIndex].choices[0];
     button2El.textContent = questions[questionIndex].choices[1];
     button3El.textContent = questions[questionIndex].choices[2];
@@ -53,5 +64,32 @@ quizContEl.addEventListener("click",function(event){
       }else{
 
       }
+      if(questionIndex < questions.length-1){
+          questionIndex++;
+          getQuestion();
+      }else{
+   
+      }
   }
 })
+
+function summary(){
+
+}
+
+function startTimerScore(){
+    scoreEl.classList.remove("hide");
+    timerEl.classList.remove("hide")
+    timerEl.textContent = "Timer :  ";
+    const spanEl = document.createElement("span")
+    timerEl.appendChild(spanEl)
+    timerObject = setInterval(()=>{
+        
+        spanEl.textContent = timer;
+        if(timer > 1){
+            timer--;
+        }else{
+
+        }
+    },1000)
+}
