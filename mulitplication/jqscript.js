71d82082918e4d9a882422fdba11d06dde6f0d32
Sanganu <kppkmsanu@gmail.com>
2022-02-27 06:15:05 -0700

    Score on page displayed

--- mulitplication/jqscript.js
+++ mulitplication/jqscript.js
@@ -8,46 +8,50 @@ const button1El = document.createElement("button");
 const button2El = document.createElement("button");
 const button3El = document.createElement("button");
 const button4El = document.createElement("button");
+const spanSEl = document.createElement("span")
+
+let questionIndex = 0;
+let timer = 3 * questions.length;
+let timerObject;
+let score = 0;
 
 function renderHTMLElements() {
-        //     const h3El = document.createElement("h3");
-        //     quizContEl.appendChild(h3El);
-        //     for (let i = 1; i <= 4; i++) {
-        //         const btn1El = document.createElement("button");
-        //         quizContEl.appendChild(btn1El);
-        //     }
-            // Quiz Container
-            quizContEl.appendChild(h4El)
-            quizContEl.appendChild(button1El);
-            quizContEl.appendChild(button2El);
-            quizContEl.appendChild(button3El);
-            quizContEl.appendChild(button4El);
-            
-            //Timers and Scores
+    //     const h3El = document.createElement("h3");
+    //     quizContEl.appendChild(h3El);
+    //     for (let i = 1; i <= 4; i++) {
+    //         const btn1El = document.createElement("button");
+    //         quizContEl.appendChild(btn1El);
+    //     }
+    // Quiz Container
+    quizContEl.appendChild(h4El)
+    quizContEl.appendChild(button1El);
+    quizContEl.appendChild(button2El);
+    quizContEl.appendChild(button3El);
+    quizContEl.appendChild(button4El);
+    h4El.classList.add("question")
+    scoreEl.classList.remove("hide");
+    timerEl.classList.remove("hide")
+    //Timers and Scores
 
 }
 
 
-let questionIndex = 0;
-let timer = 10 * questions.length;
-let timerObject;
-let score=0;
-const spanSEl = document.createElement("span")
-scoreEl.appendChild(spanSEl)
+
+
 scoreEl.classList.add("hide");
 timerEl.classList.add("hide")
 
 
 startBtnEl.addEventListener("click", function () {
-  startBtnEl.classList.add("hide")
-  renderHTMLElements();
-  getQuestion();
-  startTimerScore()
+    startBtnEl.classList.add("hide")
+    renderHTMLElements();
+    getQuestion();
+    startTimerScore()
 })
 
-function getQuestion(){
+function getQuestion() {
+    console.log(questionIndex)
     h4El.textContent = questions[questionIndex].question;
-    h4El.classList.add("question")
     button1El.textContent = questions[questionIndex].choices[0];
     button2El.textContent = questions[questionIndex].choices[1];
     button3El.textContent = questions[questionIndex].choices[2];
@@ -55,44 +59,44 @@ function getQuestion(){
 }
 
 
-quizContEl.addEventListener("click",function(event){
-  console.log("current",event.target.tagName)
-  if(event.target.tagName === 'BUTTON'){
-      console.log("button",event.target.textContent)
-      if(event.target.textContent=== questions[questionIndex].answer){
-          score+=5;
-          spanSEl.textContent = score
-      }else{
-         timer-=10;
-      }
-      if(questionIndex < questions.length-1){
-          questionIndex++;
-          getQuestion();
-      }else{
-         summary()
-      }
-  }
+quizContEl.addEventListener("click", function (event) {
+    console.log("current", event.target.tagName)
+    if (event.target.tagName === 'BUTTON') {
+        console.log("button", event.target.textContent)
+        if (event.target.textContent == questions[questionIndex].answer) {
+            score += 5;
+            spanSEl.innerText = score
+        } else {
+            timer -= 10;
+            spanSEl.innerText = score
+        }
+        if (questionIndex < questions.length - 1) {
+            questionIndex++;
+            getQuestion();
+        } else {
+            summary()
+        }
+    }
 })
 
-function summary(){
+function summary() {
     clearInterval(timerObject);
 
 }
 
-function startTimerScore(){
-    scoreEl.classList.remove("hide");
-    timerEl.classList.remove("hide")
+function startTimerScore() {
     timerEl.textContent = "Timer :  ";
     const spanEl = document.createElement("span")
     timerEl.appendChild(spanEl)
     scoreEl.textContent = "Score : "
-    timerObject = setInterval(()=>{
-        
+    scoreEl.appendChild(spanSEl)
+    timerObject = setInterval(() => {
+
         spanEl.textContent = timer;
-        if(timer > 1){
+        if (timer > 1) {
             timer--;
-        }else{
+        } else {
             summary()
         }
-    },1000)
+    }, 1000)
 }
